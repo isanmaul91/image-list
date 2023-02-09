897b72fb1f5a732ad874c407be2adb0a16b58819
Muhammad Ihsan Maula <m.ihsanmaula@gmail.com>
2023-02-09 23:36:39 +0700

    init project

---
+++ image-list/Views/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  image-list
+//
+//  Created by Muhammad Ihsan Maula on 09/02/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .white
+    }
+
+}
+
